c9629e6656bd777a0363320e92bed18e5823e7d6
sean609002 <pau22360801@gmail.com>
2024-04-02 09:12:30 +0800

    create tests for MaxArrayDeque

---
+++ proj1/test/MaxArrayTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import deque.MaxArrayDeque;
+import org.junit.Test;
+
+import java.util.Comparator;
+
+import static org.junit.Assert.assertEquals;
+
+public class MaxArrayTest {
+    @Test
+    public void MaxTest(){
+        MaxArrayDeque<Integer> test = new MaxArrayDeque<>(new MyCompare());
+        for (int i = 0;i < 100;i++) {
+            if(i % 2 == 0){
+                test.addFirst(i);
+            }else{
+                test.addLast(i);
+            }
+        }
+        assertEquals(test.max(),(Integer) 99);
+    }
+}
+class MyCompare implements Comparator<Integer>{
+
+    public int compare(Integer o1, Integer o2) {
+        if(o1 < o2){
+            return -1;
+        }else if(o1 == o2){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+}
